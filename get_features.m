88120ea2e3fc7dbce06d98120ff56c09a593a7b6
Siddat <57923667+md-sid@users.noreply.github.com>
2021-02-05 23:52:28 -0700

    updated the function

--- get_features.m
+++ get_features.m
@@ -1,12 +1,17 @@
-function [output, T] = get_features(y, Fs)
+function [output, T] = get_features(y, Fs, W, NFFT)
 %GET_FEATURES Extracts the features for Audio Events Classification.
-%   GET_FEATURES(y, Fs) returns a output of size M x N matrix, where M is
-%   the number of time segments, and N is the number of features extracted.
+%   GET_FEATURES(y, Fs, W, NFFT) takes input y (the audio signal), Fs
+%   (sampling frequency), W (window size), and NFFT (number of FFT points).
 % 
-    window_size = 2048;
+%   [OUTPUT, T] = get_features(y, Fs, W, NFFT) returns an output (the
+%   features) and time T. The output is a matrix of size M x N where M is
+%   the number of of time segments and N is the number of features
+%   extracted. T is the time vector. 
+% 
+    window_size = W;
     window = hamming(window_size);
     noverlap = window_size/2;
-    nfft = gpuArray(1024);
+    nfft = gpuArray(NFFT);
     [S,~,T,P] = spectrogram(y,window,noverlap,nfft,Fs,'yaxis');
     P = (10*log10(P))';
     [coeffs,delta] = mfcc(S,Fs,"LogEnergy","Ignore");
